b941d611d0d9bd8ddc6ae358981171c2280b7c5b
rockitweb <65389203+rockitweb@users.noreply.github.com>
2025-07-03 19:32:15 +1000

    blogs

--- src/unified-theme/components/Niba/blog-list.tsx
+++ src/unified-theme/components/Niba/blog-list.tsx
@@ -1,5 +1,5 @@
-import { HSBlog } from '../types/hs-data-types.js';
-import { BlogCard } from './cards/blog-card.js';
+import { HSBlog } from "../types/hs-data-types.js";
+import { BlogCard } from "./cards/blog-card.js";
 
 export type BlogListProps = {
   blogs: HSBlog[];
@@ -7,7 +7,7 @@ export type BlogListProps = {
 export function BlogList({ blogs }: BlogListProps) {
   return (
     <div className="latest-blogs">
-      <ul className="list-disc flex gap-5">
+      <ul className="list-disc flex  justify-center gap-5">
         {blogs?.map((blog: HSBlog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
